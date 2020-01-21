08db2f52b702ebf699a83b971daa433b94f6e390
stefan320 <stefan_014_@hotmail.com>
2020-01-21 11:12:05 +0100

    modified files for deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.33863c7316c85cfa9cfd7607061e5fd3.js"
+  "/monsters-rolodex/precache-manifest.0b8877bf9c47822ba12b19ec090f33b3.js"
 );
 
 self.addEventListener('message', (event) => {
